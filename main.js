b05cf2937d6d5dee9a2efdb44526e265059ea49a
Noha2017 <31400041+Noha2017@users.noreply.github.com>
2017-10-02 13:05:46 +1100

    HW for week 5

---
+++ main.js
@@ -0,0 +1,17 @@
+$('#submit-btn').click(changeBackground);
+
+function changeBackground(event) {
+  event.preventDefault();
+  var userInput = $('#city-type').val();
+  if (userInput === 'nyc' || userInput === 'new york') {
+    $('body').addClass('nyc');
+  } else if (userInput === 'austin') {
+    $('body').addClass('austin');
+  } else if (userInput === 'la' || userInput === 'los angeles ') {
+    $('body').addClass('la');
+  } else if (userInput === 'sf' || userInput === 'san francisco') {
+    $('body').addClass('sf');
+  } else if (userInput === 'sydney') {
+    $('body').addClass('sydney');
+  }
+}
